4d95746b857fb30d42a93b174e1838cd45482236
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-02 15:07:52 +0300

    add database connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const router = require('./router/router');
+const client = require('./database');
 
 app.use(cors({
     origin: ['*'],
@@ -9,6 +10,6 @@ app.use(cors({
 }));
 app.use(express.json({ limit: "10mb" }));
 app.use('/', router);
+client.dbConnection();
 
 app.listen(3000, () => console.log("app is running on port 3000"));
-
---
+++ database.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Client({
+    connectionString: process.env.DATABASE_CONNECTION,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+const dbConnection = async () => {
+    await client.connect()
+        .then(() => console.log('Database connected'))
+        .catch(err => console.error('Connection error', err));
+
+}
+
+
+module.exports = { dbConnection, client };
