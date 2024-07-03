15d30689d779fc86c5c85c567a2ca6da1e3f077e
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-03 10:08:30 +0300

    merge and edit auth

--- models/auth.js
+++ models/auth.js
@@ -15,11 +15,45 @@ async function register(req, res) {
 
         const result = await client.query(query);
 
-        return res.status(200).send(result);
+        return res.status(200).send(result.rows[0]);
+    } catch (e) {
+        return res.status(400).send(e.message);
+    }
+}
+
+async function login(req, res) {
+    const { username, password } = req.body;
+
+    console.log(username, password);
+
+    try {
+        const query = `SELECT * FROM "user" WHERE username = '${username}'`;
+        const result = await client.query(query);
+
+        if (result.rows.length === 0) {
+            return res.status(400).send("User was not found");
+        }
+
+        const user = result.rows[0];
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+            return res.status(400).send("Invalid Password");
+        }
+        // using JWT which are json web tokens to authorize users
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.SECRET_KEY,
+            { expiresIn: '1h' }
+        );
+
+        return res.status(200).send({ token });
     } catch (e) {
         return res.status(400).send(e.message);
     }
 }
 
 
-module.exports = { register };
+module.exports = { register, login };
