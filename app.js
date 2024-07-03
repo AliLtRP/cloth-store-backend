fd7c93b2978de28a2aa7392bc64ee83c2c33e8ce
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-03 14:35:16 +0300

    create categoryToProduct

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const router = require('./router/auth.router');
 const categoryRouter = require('./router/category.router');
 const productRouter = require('./router/product.router');
+const categoryToProductRouter = require('./router/categoryToProduct');
 const { dbConnection } = require('./database');
 
 app.use(cors({
@@ -14,6 +15,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use('/', router);
 app.use('/category', categoryRouter);
 app.use('/product', productRouter);
+app.use('/test', categoryToProductRouter);
 dbConnection();
 
 app.listen(3000, () => console.log("app is running on port 3000"));
---
+++ models/categoryToProduct.js
@@ -0,0 +1,45 @@
+const { client } = require('../database');
+
+
+async function createCategoryToProduct(req, res) {
+    const { category_id, product_id } = req.body;
+
+    console.log(category_id, product_id);
+
+    try {
+        const query = `
+        INSERT INTO "categorytoproduct" (product_id, category_id) 
+        VALUES ($1, $2)
+        RETURNING *;
+    `;
+
+        const values = [product_id, category_id];
+
+        const result = await client.query(query, values);
+
+        return res.status(200).send({
+            success: true,
+            data: result.rows[0]
+        });
+    } catch (e) {
+        return res.status(501).send({
+            success: false,
+            error: e
+        });
+    }
+}
+
+async function getCategoryToProduct(req, res) {
+
+}
+
+async function getCategoryToProduct(req, res) {
+
+}
+
+async function getCategoryToProduct(req, res) {
+
+}
+
+
+module.exports = { createCategoryToProduct }
