5a8128d6a319535ed1ee89d4a1b7c9db39c24b2f
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-04 11:43:20 +0300

    category done

--- models/category.js
+++ models/category.js
@@ -28,19 +28,91 @@ async function createCategory(req, res) {
 }
 
 async function getCategory(req, res) {
+    const { id } = req.query
 
+    try {
+        const query = ` SELECT * FROM "category" WHERE id=$1;`;
+
+        const value = [id];
+
+        const result = await client.query(query, value);
+
+        return res.status(200).send({
+            success: true,
+            data: result.rows[0]
+        });
+    } catch (e) {
+        return res.status(501).send({
+            success: false,
+            error: e
+        });
+    }
 }
 
 async function getAllCategory(req, res) {
+    try {
+        const query = ` SELECT * FROM "category"`;
 
+        const result = await client.query(query);
+
+        return res.status(200).send({
+            success: true,
+            data: result.rows
+        });
+    } catch (e) {
+        return res.status(501).send({
+            success: false,
+            error: e
+        });
+    }
 }
 
 async function updateCategory(req, res) {
+    const { id, name, img, priority, active } = req.body;
 
+    try {
+        const query = `UPDATE "category"
+        SET name=$1, img=$2, priority=$3, active=$4
+        WHERE id=${id}`;
+
+        const value = [name, img, priority, active];
+
+        const result = await client.query(query, value);
+
+        return res.status(200).send({
+            success: true,
+            data: result.rows[0]
+        });
+    } catch (e) {
+        return res.status(501).send({
+            success: false,
+            error: e
+        });
+    }
 }
 
 async function deleteCategory(req, res) {
+    const { id, status } = req.body;
 
+    try {
+        const query = `UPDATE "category"
+        SET active=$1
+        WHERE id=${id}`;
+
+        const value = [status];
+
+        const result = await client.query(query, value);
+
+        return res.status(200).send({
+            success: true,
+            data: result.rows[0]
+        });
+    } catch (e) {
+        return res.status(501).send({
+            success: false,
+            error: e
+        });
+    }
 }
 
 module.exports = { createCategory, getCategory, getAllCategory, updateCategory, deleteCategory }
