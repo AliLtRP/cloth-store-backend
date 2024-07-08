4a58bf841a37fce8c6592ed239cc95eda6e11c42
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-08 11:53:15 +0300

    remove header auth

--- router/product.router.js
+++ router/product.router.js
@@ -2,7 +2,28 @@ const express = require('express');
 const { createProduct, getProduct, getAllProducts, updateProduct, deleteProduct } = require('../models/product');
 const router = express.Router();
 
-
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: John Doe
+ */
 router.get('/', getProduct);
 router.post('/', createProduct);
 router.get('/all', getAllProducts);
