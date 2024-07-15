006372b168d93aa9c4b496b4e8a7bde572b0d2be
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-07-15 11:41:37 +0300

    edit cors

--- app.js
+++ app.js
@@ -11,13 +11,24 @@ const bannerRouter = require("./router/banner.router");
 const categoryToProductRouter = require('./router/categoryToProduct');
 const rating = require('./router/rating.router');
 const { dbConnection } = require('./database');
-const port = process.env.PORT
+const port = process.env.PORT || 5000; // Ensure default port if not set in env
+
+const allowedOrigins = [
+    'https://6694dc344b9eb44ccff31e30--spiffy-fox-78a08a.netlify.app',
+];
 
 app.use(cors({
-    origin: "https://6694dc344b9eb44ccff31e30--spiffy-fox-78a08a.netlify.app",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     exposedHeaders: ["Authorization", "Content-Type"],
 }));
+
 app.use(express.json({ limit: "10mb" }));
 app.use('/', router);
 app.use('/category', categoryRouter);
